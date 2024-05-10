0ee116a98f55777d877dbd3a1addb1c2ea6fdc1c
Tuan Nguyen <sanhtuanst1029@gmail.com>
2024-05-10 14:20:41 +0700

    add env in setup

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export DATABASE_URL="postgresql://postgres:password@localhost:5432/postgres"
+export AUTH0_CLIENT_ID=0BS0GNkORer3VVTBFxaitLDOmJ51cswe
+export AUTH0_CLIENT_SECRET=v3qLu1pz0JxzuKeSQMvjsCnrddOh3n9vshV7UqpschcrNNCZqz6zUAKk-yye5NLA
+export AUTH0_DOMAIN='dev-nst.us.auth0.com'
+export APP_SECRET_KEY=heroku-capstone
+export ALGORITHMS = ['RS256']
+export API_AUDIENCE = 'https://capstone-udacity.com'
